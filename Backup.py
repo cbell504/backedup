96774bf3dfc033957265cb32d5c9c4afc55f5d94
Christopher Bell <cbell7@icloud.com>
2020-01-14 23:09:35 -0600

    Changed update to backup, added more constants.

--- Backup.py
+++ Backup.py
@@ -9,41 +9,42 @@ import shutil
 
 def main():
     create_backupper_folder()
-    if(not did_update_today()):
-        update_directory = create_update_directory()
-        create_update(update_directory)
+    if(not did_backup_today()):
+        backup_directory = create_backup_directory()
+        create_backup(backup_directory)
+        print(Constants.DONE)
 
 
 def create_backupper_folder():
     if(os.path.isdir(Constants.DEST_FOLDER)):
-        print("The Backupper folder exist.")
+        print(Constants.FOLDER_EXIST)
         return True
     else:
         os.mkdir(Constants.DEST_FOLDER)
 
 
-def did_update_today():
+def did_backup_today():
     dest_folder_list = os.listdir(Constants.DEST_FOLDER)
     for folder in dest_folder_list:
         if(str(datetime.date.today()) in folder):
             print(Constants.OUTPUT_FOLDER_EXIST)
             return True
         else:
-            print("Starting update...")
+            print(Constants.STARTING_UPDATE)
             return False
 
 
-def create_update_directory():
-    update_directory = Constants.DEST_FOLDER + \
+def create_backup_directory():
+    backup_directory = Constants.DEST_FOLDER + \
         Constants.NEW_UPDATE_FOLDER + str(datetime.date.today())
-    if(os.path.isdir(update_directory)):
+    if(os.path.isdir(backup_directory)):
         print(Constants.OUTPUT_FOLDER_EXIST)
-    return update_directory
+    return backup_directory
 
 
-def create_update(update_directory):
+def create_backup(backup_directory):
     shutil.copytree(src=Constants.SRC_FOLDER,
-                    dst=update_directory)
+                    dst=backup_directory)
 
 
 if __name__ == "__main__":
--- Constants.py
+++ Constants.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-SRC_FOLDER = "/home/chris"
 
+DONE = "DONE!"
+FOLDER_EXIST = "The Backupper folder exist!"
 DEST_FOLDER = "/media/passport/backupper"
-
 NEW_UPDATE_FOLDER = "/backupper_"
-
 OUTPUT_FOLDER_EXIST = "An update has already occurred today!"
+SRC_FOLDER = "/home/chris"
+STARTING_UP = "Starting Backup..."
